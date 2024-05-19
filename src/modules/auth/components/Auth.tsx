d89d4a9888ddfda0a94829e1ed3de620d9170c03
MikhailMedinskiy <medinskiym@gmail.com>
2024-05-19 18:56:47 +0300

    working on inputs

--- src/modules/auth/components/Auth.tsx
+++ src/modules/auth/components/Auth.tsx
@@ -2,9 +2,10 @@ import { useAuth } from '../../../hooks/useAuth.ts';
 import { Spinner } from '../../../components/Spinner/Spinner.tsx';
 import { AppError } from '../../appError';
 import { AuthProps } from '../types.ts';
+import { Box } from '@chakra-ui/react';
 
 export const Auth = ({ children }: AuthProps) => {
-  const { isLoading, isError } = useAuth();
+  const { isLoading, isError, chatId } = useAuth();
 
   if (isLoading) {
     return <Spinner isFullHeight />;
@@ -14,5 +15,10 @@ export const Auth = ({ children }: AuthProps) => {
     return <AppError />;
   }
 
-  return children;
+  return (
+    <>
+      <Box>chatId: {chatId}</Box>
+      {children}
+    </>
+  );
 };
